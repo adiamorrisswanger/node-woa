26ab4cba1b718cc66188c9d661769464bc55e354
Adia Morris Swanger <adiamorrisswanger@gmail.com>
2022-03-06 14:41:27 -0600

    Added MongoDB database

--- workOfArtServer/app.js
+++ workOfArtServer/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const artistportalRouter = require('./routes/artistportalRouter');
 
 var app = express();
 
@@ -19,8 +20,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/artistportal', artistportalRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
